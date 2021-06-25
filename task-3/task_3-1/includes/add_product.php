ac5ad11e0fd8ed03163cde4fdf96e1f24311c392
Serge <serge@Serges-MacBook-Pro.local>
2021-06-25 14:59:04 +0200

    add 4each

--- task-3/task_3-1/includes/add_product.php
+++ task-3/task_3-1/includes/add_product.php
@@ -1,15 +1,18 @@
 <?php
 
-$title = $_POST['title'];
-$description = $_POST['description'];
-$category = $_POST['category'];
-$price = $_POST['price'];
+// $title = $_POST['title'];
+// $description = $_POST['description'];
+// $category = $_POST['category'];
+// $price = $_POST['price'];
 
-$content = "Название товара: $title\nОписание товара: $description\nКатегория товара: $category\nЦена: $price рублей\n";
+foreach ($_POST as $key => $value) {
+    $$key = $value;
+}
+
+$content = "Название товара: $title\nОписание товара: $description\nКатегория товара: $category\nЦена: $price рублей\n\n";
 
 $filename = "products.txt";
 
 $fp = fopen($filename, "a+");
 fwrite($fp, $content);
 fclose($fp);
-
